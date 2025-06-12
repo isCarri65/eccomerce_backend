2e9c468f9c89e7569351a447fd79a201984d71c0
Ismael <iscarrillo65@gmail>
2025-06-12 20:40:46 -0300

    merge con isma_dev

--- ecommerce-back/src/main/java/com/ecommerce/controllers/adminControllers/AddressController.java
+++ ecommerce-back/src/main/java/com/ecommerce/controllers/adminControllers/AddressController.java
@@ -1,9 +1,5 @@
-<<<<<<<< HEAD:ecommerce-back/src/main/java/com/ecommerce/controllers/AddressController.java
-package com.ecommerce.controllers;
-========
-package com.ecommerce.controllers.adminControllers;
->>>>>>>> isma_dev:ecommerce-back/src/main/java/com/ecommerce/controllers/adminControllers/AddressController.java
 
+package com.ecommerce.controllers.adminControllers;
 import com.ecommerce.entities.Address;
 import com.ecommerce.services.AddressService;
 import org.springframework.web.bind.annotation.RequestMapping;
