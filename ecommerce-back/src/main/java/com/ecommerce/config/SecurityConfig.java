02c75fcc89e08d0eb3ade2f51be6c7caf7f02ed2
Ismael <iscarrillo65@gmail>
2025-06-11 21:39:09 -0300

    cambios redundantes

--- ecommerce-back/src/main/java/com/ecommerce/config/SecurityConfig.java
+++ ecommerce-back/src/main/java/com/ecommerce/config/SecurityConfig.java
@@ -37,7 +37,7 @@ public class SecurityConfig {
                 .csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/auth/**").permitAll()
-                        .requestMatchers(HttpMethod.GET,"/api/**").permitAll()
+                        .requestMatchers("/api/**").permitAll()
                         .anyRequest().authenticated()
                 )
                 .sessionManagement(sess -> sess.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
