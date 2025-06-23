0ab4b181d1761738c912e88310df61515c7bde07
Ismael <iscarrillo65@gmail>
2025-06-23 01:42:58 -0300

    detalle

---
+++ ecommerce-back/src/main/java/com/ecommerce/mappers/AddressMapper.java
@@ -0,0 +1,51 @@
+package com.ecommerce.mappers;
+
+import com.ecommerce.dto.Address.AddressAdminDTO;
+import com.ecommerce.dto.Address.AddressDTO;
+import com.ecommerce.dto.Address.CreateAddressDTO;
+import com.ecommerce.dto.Address.UpdateAddressDTO;
+import com.ecommerce.entities.Address;
+import com.ecommerce.entities.User;
+import org.springframework.stereotype.Component;
+
+@Component
+public class AddressMapper implements BaseAdminMapper<Address, Long, AddressDTO, CreateAddressDTO, UpdateAddressDTO> {
+    @Override
+    public  AddressDTO toDTO(Address address) {
+        AddressDTO dto = new AddressDTO();
+        dto.setApartment(address.getApartment());
+        dto.setLocality(address.getLocality());
+        dto.setNumber(address.getNumber());
+        dto.setStreet(address.getStreet());
+        dto.setPostal(address.getPostal());
+        dto.setProvince(address.getProvince());
+        dto.setAptNumberAndFloor(address.getAptNumberAndFloor());
+        return dto;
+    }
+    @Override
+    public Address UDTOtoEntity (UpdateAddressDTO updateDTO, Long id){
+        Address address = new Address();
+        address.setProvince(updateDTO.getProvince());
+        address.setLocality(updateDTO.getLocality());
+        address.setNumber(updateDTO.getNumber());
+        address.setStreet(updateDTO.getStreet());
+        address.setPostal(updateDTO.getPostal());
+        address.setAptNumberAndFloor(updateDTO.getAptNumberAndFloor());
+        address.setApartment(updateDTO.getApartment());
+        return address;
+    }
+    @Override
+    public Address CDTOtoEntity(CreateAddressDTO addressDTO) {
+        Address address = new Address();
+        User user = new User();
+        user.setId(addressDTO.getUserId());
+        address.setApartment(addressDTO.getApartment());
+        address.setLocality(addressDTO.getLocality());
+        address.setNumber(addressDTO.getNumber());
+        address.setStreet(addressDTO.getStreet());
+        address.setPostal(addressDTO.getPostal());
+        address.setProvince(addressDTO.getProvince());
+        address.setUser(user);
+        return address;
+    }
+}
